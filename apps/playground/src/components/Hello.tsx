3283db16f168ecf3b8118f828a81aeb74e1a9afa
sparkchan <zaenxbt@gmail.com>
2025-11-16 05:28:09 +0700

    Create Hello.tsx

---
+++ apps/playground/src/components/Hello.tsx
@@ -0,0 +1,7 @@
+export default function Hello() {
+  return (
+    <div style={{ padding: 20, background: "#eee", marginTop: 20 }}>
+      <h3>Hello from the Playground Component 👋</h3>
+    </div>
+  );
+}
