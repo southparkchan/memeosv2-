1a4472b6afd61ea44083ca5060c1610c2801792d
sparkchan <zaenxbt@gmail.com>
2025-11-16 02:19:40 +0700

    Create index.tsx

---
+++ apps/pages/index.tsx
@@ -0,0 +1,10 @@
+import MemeCreator from "../components/MemeCreator";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-10">
+      <h1 className="text-4xl font-bold mb-6">Sparkchan MemeOS Playground</h1>
+      <MemeCreator />
+    </main>
+  );
+}
