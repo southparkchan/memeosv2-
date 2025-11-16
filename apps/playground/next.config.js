31b54703a013cc3b812072e2a049dde9818193b6
sparkchan <zaenxbt@gmail.com>
2025-11-16 08:09:51 +0700

    Update next.config.js

--- apps/playground/next.config.js
+++ apps/playground/next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   reactStrictMode: false
 };
 
-export default nextConfig;
-export default {
-  output: 'export',
-  trailingSlash: true
-};
+module.exports = nextConfig;
